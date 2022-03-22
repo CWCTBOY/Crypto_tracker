114106e16b9380447b235b42294a69851e079d29
CWCTBOY <qkrdlswo98@gmail.com>
2022-03-23 02:58:37 +0900

    nothing

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path='/' component={Home} />
+        <Route path="/" component={Home} />
       </Switch>
     </Router>
   );
